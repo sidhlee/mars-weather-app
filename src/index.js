590089f5461af366fc298765c1ab17de9b225bd7
Hayoun Lee <sidhlee@gmail.com>
2020-04-04 22:09:47 +0900

    Add style for bottom drawer: previous 7 days

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import "./styles/main.scss";
+
+// For CSS dev purpose
+const previousWeatherToggle = document.querySelector(".show-previous-weather");
+const previousWeather = document.querySelector(".previous-weather");
+
+previousWeatherToggle.addEventListener("click", () => {
+  previousWeather.classList.toggle("show-weather");
+});
